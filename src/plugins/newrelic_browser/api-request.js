1804a1a04cf29913708655dcf78ac9042966b89e
guilhermecarneiroo <guilhermecarneiroo@gmail.com>
2018-10-18 13:41:27 -0300

    configuring dockerfile

--- src/plugins/newrelic_browser/api-request.js
+++ src/plugins/newrelic_browser/api-request.js
@@ -7,19 +7,22 @@ const collectData = (names, values) => new Promise((resolve, reject) => {
   const myStartDate = new Date(myDate - MS_PER_MINUTE);
   const API_DOMAIN = 'https://api.newrelic.com';
 
-  const API_VERSION = 'v2';
-  const REQUEST_URL = `${API_DOMAIN}/${API_VERSION}/applications/${config.APP_ID}/metrics/data.json`;
-  const options = {
-    url: REQUEST_URL,
-    headers: {
-      'X-Api-Key': `${config.API_KEY}`,
-    },
-    form: {
-      from: myStartDate.toJSON().split('.')[0],
-      to: myDate.toJSON().split('.')[0],
-      period: 60,
-    },
-  };
+    const API_VERSION = 'v2';
+    const REQUEST_URL =
+      `${API_DOMAIN}/${API_VERSION}/applications/${process.env.APP_ID}/metrics/data.json`;
+    const options = {
+      url: REQUEST_URL,
+      headers: {
+        'X-Api-Key': `${process.env.API_KEY}`
+      },
+      form: {
+        from: myStartDate.toJSON().split('.')[0],
+        to: myDate.toJSON().split('.')[0],
+        period: 60
+      }
+    };
+    names && (options.form.names = names);
+    values && (options.form.values = values);
 
   if (names) {
     options.form.names = names;
